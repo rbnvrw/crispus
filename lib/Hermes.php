d55f6aed12454ebd798433ee113e2162bf483ea9
Ruben Verweij <development@rubenverweij.nl>
2014-12-01 22:24:13 +0100

    Create initial directory structure

---
+++ lib/Hermes.php
@@ -0,0 +1,19 @@
+<?php
+namespace RubenVerweij;
+
+/**
+ * Hermes CMS
+ *
+ * @author Ruben Verweij
+ * @link http://rubenverweij.nl
+ * @license http://opensource.org/licenses/MIT
+ * @version 0.1
+ */
+class Hermes {
+
+    public function __construct()
+    {
+    
+    }
+
+}
