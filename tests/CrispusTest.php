eb47b63fca629a418ac0da24ed2054f59e7babe3
development@rubenverweij.nl <development@rubenverweij.nl>
2015-01-17 17:40:56 +0100

    Set root path for test

--- tests/CrispusTest.php
+++ tests/CrispusTest.php
@@ -4,11 +4,10 @@ class CrispusTest extends PHPUnit_Framework_TestCase
 {
 
     private $oCrispus;
+	private $sRootPath;
     
-    public function setUp(){
-        if(!defined('ROOT_PATH')){
-            define('ROOT_PATH', realpath('../'.dirname(__FILE__)));
-        }
+    public function setUp(){		
+		$this->sRootPath = (defined('ROOT_PATH') && strlen(ROOT_PATH) > 0) ? ROOT_PATH : realpath('../'.dirname(__FILE__));
     }
     
     public function tearDown(){
@@ -20,7 +19,7 @@ class CrispusTest extends PHPUnit_Framework_TestCase
      */
     public function testRenderPage(){
 		// Test if the "About" page is correctly routed and passed through Markdown and Twig
-        $this->oCrispus = new Crispus\Crispus(ROOT_PATH.'/config/test_config_about.json');
+        $this->oCrispus = new Crispus\Crispus($this->sRootPath.'/config/test_config_about.json');
         
         // Expect a non-empty body tag, with <h1>About Crispus CMS</h1> in the body
         $this->expectOutputRegex('/\<body.*(?=\>)>(.*\<h1\>About Crispus CMS\<\/h1\>.*)(?=\<\/body>)/sU');
@@ -31,7 +30,7 @@ class CrispusTest extends PHPUnit_Framework_TestCase
      */
 	public function testConfig(){
 		// Request the about page
-		$this->oCrispus = new Crispus\Crispus(ROOT_PATH.'/config/config.json');
+		$this->oCrispus = new Crispus\Crispus($this->sRootPath.'/config/config.json');
 		
 		// Test if config values can be successfully retrieved
 		$sMd = $this->oCrispus->config('crispus', 'content_extension');
@@ -43,7 +42,7 @@ class CrispusTest extends PHPUnit_Framework_TestCase
      */
 	public function testNotFoundPage(){
 		// Test if the 404 not found page is correctly routed and passed through Markdown and Twig
-        $this->oCrispus = new Crispus\Crispus(ROOT_PATH.'/config/test_config_404.json');
+        $this->oCrispus = new Crispus\Crispus($this->sRootPath.'/config/test_config_404.json');
         
         // Expect a non-empty body tag, with <h1>404: Not found</h1> in the body
         $this->expectOutputRegex('/\<body.*(?=\>)>(.*\<h1\>404\: Not found\<\/h1\>.*)(?=\<\/body>)/sU');
