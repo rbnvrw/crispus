3aa5db7c2080dfb48e63ffd64374aa733029c5ee
development@rubenverweij.nl <development@rubenverweij.nl>
2015-01-24 17:31:29 +0100

    Add test data, Filesystem & SiteConfig tests

--- tests/CrispusTest.php
+++ tests/CrispusTest.php
@@ -14,13 +14,30 @@ class CrispusTest extends PHPUnit_Framework_TestCase
         unset($this->oCrispus);
     }
 	
-	public function testOpenFile(){
-		// Test Filesystem->getFileContents
-		
+	public function testFilesystem(){
+		// Test Filesystem->getFileContents()		
 		$oFilesystem = new Crispus\Filesystem();
-		$sContent = $oFilesystem->getFileContents($this->sRootPath.'/tests/resources/data/getFileContentsTest.txt');
-		
+		$sContent = $oFilesystem->getFileContents($this->sRootPath.'/tests/resources/data/getFileContentsTest.txt');		
 		$this->assertEquals('success', $sContent);
+		
+		// Test Filesystem->getFiles()
+		$aFiles = $oFilesystem->getFiles($this->sRootPath.'/tests/resources/data/test_list_files/', 'txt');		
+		$this->assertEquals(array('one.txt', 'two.txt', 'three.txt'), $sContent);
 	}
 	
+	public function testSiteConfig(){
+		$oConfig = new Crispus\SiteConfig($this->sRootPath.'/tests/resources/config/test_site_config.json');
+		
+		// Test Config->get()
+		$this->assertEquals('crisp', $oConfig->get('site', 'theme'));
+		
+		// Test SiteConfig->getBaseUrl()
+		$this->assertEquals('http://rubenverweij.nl', $oConfig->getBaseUrl());
+		
+		// Test SiteConfig->getUrl()
+		$this->assertEquals('http://rubenverweij.nl/vendor', $oConfig->getUrl('vendor'));
+		
+		// Test SiteConfig->getPath()
+		$this->assertEquals($this->sRootPath.'/vendor', $oConfig->getPath('vendor'));		
+	}
 }
